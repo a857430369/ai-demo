0cd7db71afcd86dfcc8663606ca0a7e3264f93ef
Andy <jiaxiao072@163.com>
2025-03-06 14:09:49 +0800

    init

---
+++ src/views/report-management/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <n-card title="销售报表">
+    <v-chart class="h-[400px]" :option="chartOption" autoresize />
+  </n-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { BarChart } from 'echarts/charts'
+import { GridComponent, TooltipComponent } from 'echarts/components'
+import VChart from 'vue-echarts'
+
+use([CanvasRenderer, BarChart, GridComponent, TooltipComponent])
+
+const chartOption = ref({
+  xAxis: {
+    type: 'category',
+    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  tooltip: {
+    trigger: 'axis'
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar',
+      showBackground: true,
+      backgroundStyle: {
+        color: 'rgba(180, 180, 180, 0.2)'
+      }
+    }
+  ]
+})
+</script>
